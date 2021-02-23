2df3f9cb139ecdfe603017b2571397cfc5f04c8e
Eric Amundsen <eric.amundsen@gmail.com>
2021-02-22 20:17:17 -0600

    Add comments

--- AdafruitMuteSwitch.ino
+++ AdafruitMuteSwitch.ino
@@ -1,25 +1,50 @@
+//////////////////////////////////////////////////////////////////////
+//
+// AdafruitMuteSwitch
+// Monitor pins on a Adafruit Flora, when state change (1->0 or 0->) output
+// a keyboard key combination to facilitate an action in Microsoft Teams
+//
+// Author : Eric Amundsen
+// Creation Date : 2021-02-19
+
 #include <Keyboard.h>
 
+//////////////////////////////////////////////////////////////////////
+// PIN DEFINITION
 #define AMUTE_SWITCH 10
 #define VMUTE_SWITCH 9
 #define HAND_SWITCH  6
 
+//////////////////////////////////////////////////////////////////////
+// CHARACTER DEFINITION
 #define AMUTE_CHAR 'm'
 #define VMUTE_CHAR 'o'
 #define  HAND_CHAR 'k'
 
-
+//////////////////////////////////////////////////////////////////////
+// states to keep track of pin state and create action during transition
 typedef enum states {S_IDLE, S_ZERO, S_ONE, S_XFER};
 
+//////////////////////////////////////////////////////////////////////
+// Globals
+// Initialize the 3 pins' state machines
 states amute_state = S_IDLE;
 states vmute_state = S_IDLE; 
 states hand_state  = S_IDLE; 
 
-int buttonState(int button, states *st_ptr) {
+//////////////////////////////////////////////////////////////////////
+// 
+int buttonState(  // return 1 when in XFER state, 0 otherwise
+  int button,     // the pin to read
+  states *st_ptr) // the state of the pin, updated herein
+  {
 
-  // return value, only true upon release of the button
   int rv = 0;
 
+  // start in IDLE, move to S_ZERO if pin at 0, S_ONE if pin at 1
+  // stay in S_ZERO if pin at 0, stay in S_ONE if pin at 1
+  // move to S_XFER when pin doesn't match state return 1
+  // when in S_XFER move to appropiate state (pin == 0, S_ZERO)
   switch (*st_ptr) {
     case S_IDLE : if (digitalRead(button) == 0) *st_ptr = S_ZERO;
                   else                          *st_ptr = S_ONE;
@@ -54,6 +79,7 @@ void setup() {
 
 }
 
+// press modifier keys and write character
 void sendToggle(char chr, int shift, int ctrl, int alt, int gui) {
 
   if (shift) Keyboard.press(KEY_LEFT_SHIFT);
@@ -65,9 +91,11 @@ void sendToggle(char chr, int shift, int ctrl, int alt, int gui) {
   Keyboard.releaseAll();
 }
 
+// call state machine and if return 1 send character
 void checkSwitch(int swtch, char chr, states *s_ptr) {
   if (buttonState(swtch, s_ptr)) {
-    sendToggle(chr, 1, 1, 0, 0);
+    sendToggle(chr, 1, 1, 0, 0);  // TODO this assumes all characters use shift-ctrl modifier,
+                                  // find elegant way to not do this
   }
 }
 
